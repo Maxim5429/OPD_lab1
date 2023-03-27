1050ce9f6be35327197820cb2eda5bc6724555d1
Maxim_Petrov <max@itproger.com>
2023-03-28 00:25:59 +0600

    получил список цен

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ print(page.status_code) # смотрим ответ
 soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
 block = soup.findAll('span', class_='ProductPrice__price ProductCardHorizontal__price__price')  # находим  контейнер с нужным классом
 price=[]
-
+for data in block: # проходим циклом по содержимому контейнера
+    price.append(data.find('span',class_='ProductCardHorizontal__price_current-price js--ProductCardHorizontal__price_current-price').text.strip()) # находим тег <span> и добавляем цену в список
+for i in price:
+    print(i)
