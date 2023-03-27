ab9274b011bdf528e2d9219bb869e2d6fc57cc02
Maxim_Petrov <max@itproger.com>
2023-03-27 23:27:41 +0600

    нашёл контейнер с нужным классом

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import requests # импортируем библиотеку requests
 url = 'https://www.citilink.ru/catalog/smartfony/APPLE/' # передаем необходимы URL адрес
 page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
 print(page.status_code) # смотрим ответ
+soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
+block = soup.findAll('span', class_='ProductPrice__price ProductCardHorizontal__price__price')  # находим  контейнер с нужным классом
+price=[]
+
